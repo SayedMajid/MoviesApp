9a26445d065d309b8b00f349ed0c2aa1cd565851
Majid Sayed <sydmajid04@gmail.com>
2022-04-25 10:29:03 +0530

    progress

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
 // Store the wallet amount to your local storage with key "amount"
+let wMoney = JSON.parse(localStorage.getItem('amount'))
+function addMoney(){
+    event.preventDefault()
+    let money = document.getElementById('amount').value
+    money = Number(money)
+    let wallet = document.getElementById('wallet')
+
+    wallet.textContent = money
+    localStorage.setItem('amount', JSON.stringify(money))
+
+    
+}
+
+function goToMovies(){
+    window.location.href = 'movies.html'
+}
